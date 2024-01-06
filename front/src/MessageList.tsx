cae15b83fcbddecc7f9f218c2c6adc3651a9b48a
Osimjon <99245667+Osimjon@users.noreply.github.com>
2024-01-06 08:46:54 +0300

    scroll added last

--- front/src/MessageList.tsx
+++ front/src/MessageList.tsx
@@ -1,53 +1,85 @@
-import React from "react";
-import {useQuery} from "react-query";
-import {List, ListItem, Paper} from "@mui/material/";
-import {Avatar, Box, Container} from "@mui/material";
-import {lightBlue} from "@mui/material/colors";
+import { useQuery } from "react-query";
+import { List, ListItem, Paper } from "@mui/material/";
+import { Avatar, Box, Button, Container } from "@mui/material";
+import { lightBlue } from "@mui/material/colors";
+import { ArrowDownwardRounded } from "@mui/icons-material";
+import { useRef } from "react";
+import './MessageList.css';
 
 type messageType = {
     id: string,
     message: string,
-    userName: string
-    messageTime: string
-}
+    userName: string,
+    messageTime: string,
+};
 
 export function MessageList() {
     const fetchMessages = async () => {
         const res = await fetch("http://localhost:8080/chat/messages");
         return res.json();
-    }
+    };
 
-    const {data: messages, isLoading} = useQuery({
+    const { data: messages, isLoading } = useQuery({
         queryFn: async () => {
             return fetchMessages();
-
         },
-        queryKey: ["data"]
-    })
-    if (isLoading) return (
-        <div>Loading...</div>
-    )
+        refetchInterval: 2000,
+        queryKey: ["data"],
+    });
+
+    const listRef = useRef<HTMLUListElement | null>(null);
+
+    const scrollDown = () => {
+        if (listRef.current) {
+            listRef.current.scrollTop = listRef.current.scrollHeight;
+        }
+    };
+
+    if (isLoading) return <div>Loading...</div>;
 
     return (
-
         <div>
-            <Container sx={{minHeight: "80vh"}}>
-                <List sx={{m: 3}} >
+            <Container sx={{ width: '65%' }}>
+                <List
+                    ref={listRef}
+                    className="smooth-scroll-list"
+                    sx={{
+                        m: 3,
+                        position: 'relative',
+                        overflow: 'auto',
+                        width: '80%',
+                        maxHeight: '70vh',
+                    }}
+                >
                     {messages?.map((message: messageType) => (
                         <ListItem key={message.id}>
-                            <Paper elevation={1} sx={{p: 1,paddingLeft:2 , display: 'inline-flex', paddingRight: 3}}>
-                                <Avatar sx={{ maxHeight: 27, maxWidth:27, bgcolor:lightBlue[400] }}>{message.userName[0]}</Avatar>
-                                <Box sx={{marginLeft: 2}}>
+                            <Paper
+                                elevation={1}
+                                sx={{ p: 1, paddingLeft: 2, display: 'inline-flex', paddingRight: 3 }}
+                            >
+                                <Avatar
+                                    sx={{
+                                        maxHeight: 27,
+                                        maxWidth: 27,
+                                        bgcolor: lightBlue[400],
+                                    }}
+                                >
+                                    {message.userName[0]}
+                                </Avatar>
+                                <Box sx={{ marginLeft: 2 }}>
                                     {message.userName}
-                                    <br/>
+                                    <br />
                                     {message.message}
                                 </Box>
                             </Paper>
                         </ListItem>
                     ))}
                 </List>
+                <Button onClick={scrollDown}>
+                    <ArrowDownwardRounded color={'action'} />
+                </Button>
             </Container>
         </div>
-    )
-
+    );
 }
+
--- front/src/TextBar.tsx
+++ front/src/TextBar.tsx
@@ -1,14 +1,52 @@
-import React from "react";
-import {Box, Button, Container, Input, useTheme} from "@mui/material";
+import React, {ChangeEvent, useState} from "react";
+import {Button, Container, Input, useTheme} from "@mui/material";
 import SendIcon from '@mui/icons-material/Send';
+import {useMutation, useQueryClient} from "react-query";
+import {Simulate} from "react-dom/test-utils";
+import error = Simulate.error;
 
-export function TextBar(){
+export function TextBar() {
+
     const theme = useTheme();
 
-    return<div>
+    const [message, setMessage] = useState("");
+    const [userName, setUserName] = useState("Admin");
+    const queryClient = useQueryClient();
+
+    const sendMessageMutation = useMutation(
+        async (data: { message: string; userName: string }) => {
+            return await fetch("http://localhost:8080/chat/messages", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data)
+            });
+        },
+        {
+            onSuccess: async () => {
+                //
+            },
+            onError: ()=>{
+                //
+            }
+        }
+    )
+
+
+    const handleSendMessage = () => {
+        sendMessageMutation.mutate({message, userName,});
+        setMessage("");
+    };
+
+    const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+        return setMessage(e.currentTarget.value);
+    }
+
+    return <div>
         <Container maxWidth="xl">
-            <Input style={{ width: "50%", height: "40px" }}  />
-            <Button variant="contained">
+            <Input style={{width: "50%", height: "40px"}} onChange={handleInput} value={message}  autoFocus />
+            <Button variant="contained" onClick={handleSendMessage}>
                 <SendIcon/>
             </Button>
         </Container>
